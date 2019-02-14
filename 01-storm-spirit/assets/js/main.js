f98970c5e453460917ae33dc698f5521d9ddd424
Leonardo Alves <leoves.oliveira@gmail.com>
2019-02-14 15:54:40 -0200

    [STORM-SPIRIT] refactor js code

--- 01-storm-spirit/assets/js/main.js
+++ 01-storm-spirit/assets/js/main.js
@@ -1,17 +1,7 @@
-function playSoundPressKey(e) {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const skill = document.querySelector(`.skill[data-key="${e.keyCode}"]`);
-  if(!audio) return;
-  audio.currentTime = 0;
-  audio.play();
-  skill.classList.add('playing');
-}
-
-function playSoundClick() {
-  const button = this.getAttribute('data-key');
-  const audio = document.querySelector(`audio[data-key="${button}"]`);
-  const skill = document.querySelector(`.skill[data-key="${button}"]`);
-  if(!audio) return;
+function playSound(key) {
+  const audio = document.querySelector(`audio[data-key="${key}"]`);
+  const skill = document.querySelector(`.skill[data-key="${key}"]`);
+  if (!audio) return;
   audio.currentTime = 0;
   audio.play();
   skill.classList.add('playing');
@@ -24,7 +14,12 @@ function removeTransition(e) {
 
 const skills = document.querySelectorAll('.skill');
 
-window.addEventListener('keydown', playSoundPressKey);
-skills.forEach(skill => skill.addEventListener('click', playSoundClick));
+window.addEventListener('keydown', e => {
+	playSound(e.keyCode);
+});
+
+skills.forEach(skill => skill.addEventListener('click', () => {
+	playSound(skill.getAttribute('data-key'));
+}));
 
 skills.forEach(skill => skill.addEventListener('transitionend', removeTransition));
