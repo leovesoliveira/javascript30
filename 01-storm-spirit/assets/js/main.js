ea36f625f3617f06b35f56d2c9baa30e8b04e125
Leonardo Alves <leoves.oliveira@gmail.com>
2019-02-14 14:36:53 -0200

    [DAY1] add event click to mobile

--- 01-storm-spirit/assets/js/main.js
+++ 01-storm-spirit/assets/js/main.js
@@ -1,4 +1,4 @@
-function playSound(e) {
+function playSoundPressKey(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const skill = document.querySelector(`.skill[data-key="${e.keyCode}"]`);
   if(!audio) return;
@@ -6,11 +6,25 @@ function playSound(e) {
   audio.play();
   skill.classList.add('playing');
 }
+
+function playSoundClick() {
+  const button = this.getAttribute('data-key');
+  const audio = document.querySelector(`audio[data-key="${button}"]`);
+  const skill = document.querySelector(`.skill[data-key="${button}"]`);
+  if(!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  skill.classList.add('playing');
+}
+
 function removeTransition(e) {
   if (e.propertyName !== 'transform') return;
   this.classList.remove('playing');
 }
 
 const skills = document.querySelectorAll('.skill');
+
+window.addEventListener('keydown', playSoundPressKey);
+skills.forEach(skill => skill.addEventListener('click', playSoundClick));
+
 skills.forEach(skill => skill.addEventListener('transitionend', removeTransition));
-window.addEventListener('keydown', playSound);
